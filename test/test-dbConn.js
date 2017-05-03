1e2485a9babcae2743b7ad983d0c280137cdbe88
Mochu <mochu@tuta.io>
2017-05-03 08:24:06 +0800

    adds database connection tests

---
+++ test/test-dbConn.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var mongoose = require('mongoose');
+var mlab = require('../mlab-user-pass.js');
+
+describe('Mlab-db-login-details', function(){
+    before(function(){//before the test begins
+        //nothing to setup
+    });
+
+    after(function(){// after all the test have finished
+        mongoose.connection.close();
+    });
+
+    beforeEach(function(){//run before each test
+        //nothig to setup
+    })
+
+    //run tests
+    it('Imports the user details from the mlab-user-pass.js file', function(){
+        assert.equal(mlab.user, 'matt', 'The user name should be matt');
+    });
+    it('Imports the password details from the mlab-user-pass.js file', function(){
+        assert.equal(mlab.pass, 'password', 'The user name should be matt');
+    });
+});
