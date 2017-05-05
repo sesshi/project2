723ac2eb312378f7fc1bc53cf0bdf3c4b93fa87f
mseneque <mattseneque@gmail.com>
2017-05-05 14:37:46 +0800

    adds require for app_api routes

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+var routes = require('./app_server/routes/index');
+var routesAPI = require('./app_api/routes/index');
+// var users = require('./app_server/routes/users');
 
 // import database
 require('./app_server/models/db');
@@ -25,8 +26,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
