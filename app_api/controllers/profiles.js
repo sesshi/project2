9227098b31f48dd678412ad1f8e53c4ba3b9b36c
mseneque <mattseneque@gmail.com>
2017-05-08 11:10:18 +0800

    adds post to profile controller

--- app_api/controllers/profiles.js
+++ app_api/controllers/profiles.js
@@ -7,10 +7,29 @@ var sendJsonResponse = function(res, status, content) {
     res.json(content);
 };
 
+/* POST */
+// api/profiles
 module.exports.profilesCreate = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    profile.create({
+        username: req.body.username,
+        dateOfBirth: req.body.dateOfBirth,
+        gender: req.body.gender,
+        aboutMe: req.body.aboutMe,
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        // Create arrays, split by comma.
+        interests: req.body.interests.split(","),
+        gymMemberships: req.body.gymMemberships.split(",")
+    // Supply Callback function providing success and failure
+    }, function(err, profile) {
+        if (err) {
+            sendJsonResponse(res, 400, err);
+        } else {
+            sendJsonResponse(res, 200, profile);
+        }
+    });
 };
 
+/* GET */
 // api/profiles/:userid     (try: localhost:3000/api/profiles/590f1cd3f36d281fc3b965c1)
 module.exports.profilesReadOne = function (req, res) {
     if (req.params && req.params.userid) { //Check that profileid exists in the request parameters
